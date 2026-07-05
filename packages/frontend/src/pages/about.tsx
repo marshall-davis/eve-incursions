0afd2c1c34d29e59bc040d94c915f29730d3b61b
Marshall Davis <marshall@exposuresoftware.com>
2026-07-05 19:10:45 -0400

    Use my Info

--- packages/frontend/src/pages/about.tsx
+++ packages/frontend/src/pages/about.tsx
@@ -5,7 +5,7 @@ export default function About() {
   const [mail, setMail] = useState('');
 
   useEffect(() => {
-    setMail('lars.naurath@gmail.com');
+    setMail('toothlessrebel@gmail.com');
   }, []);
 
   return (
@@ -14,7 +14,7 @@ export default function About() {
       <p>If you have any feedback, bugs, questions or feature requests, you can contact me through the following channels.</p>
       <dl className={styles.contact}>
         <dt>Ingame</dt>
-        <dd>MikeRoni <span className={styles.note}>(currently not subbed — use another method to reach me)</span></dd>
+        <dd>Rackar al'Ulqua <span className={styles.note}></span></dd>
 
         <dt>Email</dt>
         <dd>{mail ? <a href={`mailto:${mail}`}>{mail}</a> : <span className={styles.note}>loading…</span>}</dd>
