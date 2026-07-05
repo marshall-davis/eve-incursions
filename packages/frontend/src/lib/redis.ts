a3130a768e7c27fef17ef7de79703e9a6fd42bc8
Marshall Davis <marshall@exposuresoftware.com>
2026-07-05 17:58:49 -0400

    Timeout Redis

--- packages/frontend/src/lib/redis.ts
+++ packages/frontend/src/lib/redis.ts
@@ -1,3 +1,8 @@
 import Redis from 'ioredis';
 
-export const redis = new Redis({ host: process.env.REDIS_HOST || 'redis' });
+export const redis = new Redis({
+    host: process.env.REDIS_HOST || 'redis',
+    connectTimeout: 5000,
+    commandTimeout: 5000,
+    lazyConnect: true,
+});
